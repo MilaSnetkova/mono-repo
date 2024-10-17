d6e8df678f0de46812b55beba9009108e69298bc
Liudmila Snetkova <milavaleri@yandex.ru>
2024-10-17 18:01:13 +0300

    Задание в #Task 3.1

--- cmd/task-03/main.go
+++ cmd/task-03/main.go
@@ -1,33 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
- var basePrice = 100
+var basePrice = 100  
 
-func getPrice(city string, date string, celsius int) int {
-	price := basePrice
+func getTemp(city string, date string) float64 {
+	if city == "Moscow" && date == "2024-12-01" {
+	return 2.0
+}
+return 10.0 
+} 
+
+func getPrice(temp float64) float64 {
+	price := basePrice 
 	//если больше 10 градусов 
-	if celsius > 10 {
-		price = basePrice
-	}
-	//если равно 10 градусов
-	if celsius == 10 {
-		price = basePrice +10 
+	if temp > 10 {
+		return float64(price)
+	} else if temp == 10 {
+		return float64(price + 10)
 	} else {
 		// если температура ниже 10 градусов
 		// прибавляем к стоимости по 5  за каждые -2 градуса ниже 10
-		price += 10 + (5*((10 - celsius)/2))
-	}
-	return price
+		extra := 10 + ((10 - temp) / 2) * 5 
+		return float64(price) + extra		 
 }
-
+}
 func main() {
-	city:= "Moscow"
-	date := time.Date(2024, time.December, 1, 0, 0, 0, 0, time.UTC)
-	celsius := 2 
-	price := getPrice(city, date.Format("2006-01-02"), celsius)
-	fmt.Printf("Цена за кофе в %s на %s будет %d\n", city, date.Format("2006-01-02"), price)
+	city := "Moscow"
+	date := "2024-12-01"
+
+	temp := getTemp(city,date)
+	finalPrice := getPrice(temp)
+
+	fmt.Printf("Цена за кофе в %s на %s будет %.2f", city, date, finalPrice) 
 }
